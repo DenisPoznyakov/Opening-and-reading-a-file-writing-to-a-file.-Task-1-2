ccf8f4f6997163a229fa74856568effe052ee34b
Denis Poznyakov <flint037@gmail.com>
2023-09-15 17:14:55 +0300

    commit

--- main.py
+++ main.py
@@ -20,7 +20,12 @@ def get_shop_list_by_dishes(dishes, person_count):
         dish.append(cook_book[i])
     for j in dish:
         for i in j:
-            ingrediend_to_cooking[i['ingredient_name']] = {'measure': (i['measure']), 'quantity': (int(i['quantity']) * person_count)}
+            if i['ingredient_name'] not in ingrediend_to_cooking:
+                ingrediend_to_cooking[i['ingredient_name']] = {'measure': (i['measure']), 'quantity': (int(i['quantity']) * person_count)}
+            else:
+                q = int(i['quantity']) * person_count
+                ingrediend_to_cooking[i['ingredient_name']] = {'measure': (i['measure']),
+                                                               'quantity': (q + int(i['quantity']) * person_count)}
     return ingrediend_to_cooking
 
 print(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
